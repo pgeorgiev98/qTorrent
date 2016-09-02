65ab80a738a4e02b669f8963a38a7175c5faf23d
Petko Georgiev <petko.vas.georgiev@gmail.com>
2016-09-02 09:04:50 +0300

    Add main.cpp file

--- main.cpp
+++ main.cpp
@@ -1,3 +1,20 @@
-int main() {
-	return 0;
+#include "qtorrent.h"
+#include <QTextStream>
+#include <QApplication>
+
+int main(int argc, char *argv[]) {
+	QApplication app(argc, argv);
+	QTextStream err(stderr);
+	QTextStream out(stdout);
+	if(argc <= 1) {
+		err << "Please enter a filename as a command line argument" << endl;
+		return 0;
+	}
+	QTorrent qTorrent;
+	if(!qTorrent.addTorrent(argv[1])) {
+		err << "Failed to add torrent" << endl;
+	} else {
+		err << "Torrent added" << endl;
+	}
+	return app.exec();
 }
